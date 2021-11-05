d819922b2baab62937c8dee64e4177ef10633719
nz84200 <florent.dubois@airbus.com>
2021-11-05 17:23:38 +0100

    add loggor functionnalities

---
+++ src/logger.ts
@@ -0,0 +1,90 @@
+import chalk from 'chalk';
+
+const Level:Record<string, (msg:string)=>void>= {
+  'INFO': chalk.white,
+  'WARNING': chalk.yellow,
+  'ERROR': chalk.bold.red,
+  'FIX': chalk.bold.cyan,
+  'SUCCESS': chalk.green,
+};
+
+
+/**
+ * Class to log messages
+ */
+export default class Logger {
+  /**
+   * Log info message
+   * @param {string} message message to log
+   */
+  public info(message:string):void {
+    const mes = buildMessage('INFO', message);
+    console.info(mes);
+    // console.info(timestamp(), this._info(message));
+  }
+
+  /**
+   * Log try message
+   * @param {string} message message to log
+   */
+  public try(message:string):void {
+    const mes = buildMessage('INFO', message);
+    process.stdout.write(mes + ' ... \r');
+  }
+
+  /**
+   * Log warning message
+   * @param {string} message message to log
+   */
+  public warn(message:string):void {
+    const mes = buildMessage('WARNING', message);
+    console.warn(mes);
+  }
+
+  /**
+   * Log error message
+   * @param {string} message message to log
+   */
+  public error(message:string):void {
+    const mes = buildMessage('ERROR', message);
+    console.error(mes);
+  }
+
+  /**
+   * Log success message
+   * @param {string} message message to log
+   */
+  public success(message:string):void {
+    const mes = buildMessage('INFO', message);
+    console.info(mes + Level['SUCCESS'](' SUCCESS'));
+  }
+
+  /**
+   * log failure message
+   * @param {string} message message to log
+   */
+  public failure(message:string):void {
+    const mes = buildMessage('INFO', message);
+    console.info(mes + Level['ERROR'](' FAILURE'));
+  }
+
+  /**
+   * log resolution message
+   * @param {string} message resolution message
+   */
+  public resolution(message:string):void {
+    const mes = buildMessage('FIX', message);
+    console.info(mes);
+  }
+}
+
+/**
+ * Build the logger message
+ * @param {string} level message level
+ * @param {string} message main message
+ * @return {string} builded message
+ */
+function buildMessage(level:string, message:string):string {
+  const msg = Level[level](`[${level}] - ${message}`);
+  return `${new Date().toLocaleString()} - ${msg}`;
+}
